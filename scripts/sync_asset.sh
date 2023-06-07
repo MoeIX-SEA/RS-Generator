169b72b29c3df2ba4c59e389e2dbef186178afc4
日下部 詩 <github@kskb.eu.org>
2023-06-07 20:27:22 +0800

    Update sync_asset.sh

--- scripts/sync_asset.sh
+++ scripts/sync_asset.sh
@@ -14,5 +14,5 @@ cd /root/arouteserver/cache
 #python3 /root/gitrs/RIPE-AS-SET-SYNC/AS-KSKB-IX-RS2.py
 
 export AS_SET="AS210979:AS-MOEIX-SEA-DOWNSTREAM"
-export CLIENTS_ASSET_PATH="$GITHUB_WORKSPACE/output/files/kskbix-rs2-estab.yaml"
+export CLIENTS_ASSET_PATH="$GITHUB_WORKSPACE/output/kskbix-rs2-estab.yaml"
 python3 /root/gitrs/RIPE-AS-SET-SYNC/AS-KSKB-IX-RS2.py --flat
